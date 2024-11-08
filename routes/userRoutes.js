00c4efe531a880c3801a3383c985cbee2c1bfcc8
Guan Junming <guan.junming85@gmail.com>
2024-11-08 16:59:21 +0800

    add user signup, login

---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const authController = require("../controllers/authController");
+const catchAsync = require("../utils/catchAsync");
+const checkAuth = require("../middleware/checkAuth");
+
+const router = express.Router();
+
+router.post("/signup", catchAsync(authController.signup));
+router.post("/login", catchAsync(authController.login));
+
+router.use(checkAuth);
+
+module.exports = router;
