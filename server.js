f4447b637400de0d262c7eba00ef4984523aabcd
Guan Junming <guan.junming85@gmail.com>
2024-11-13 20:31:35 +0800

    add helmet, express-rate-limit

--- server.js
+++ server.js
@@ -2,22 +2,33 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./db/db");
 const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const CustomError = require("./utils/customError");
 const restaurantsRoutes = require("./routes/restaurantsRoutes");
 const userRoutes = require("./routes/userRoutes");
-const CustomError = require("./utils/customError");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 connectDB();
 const app = express();
 
 app.use(cors());
-app.options("*", cors());
-
+app.use(helmet());
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// routes
 app.use("/api/restaurants", restaurantsRoutes);
 app.use("/api", userRoutes);
 
+// route not found
 app.use((req, res, next) => {
   const error = new CustomError("Can't find route on server.", 404);
   next(error);
@@ -31,7 +42,7 @@ app.use((err, req, res, next) => {
     .json({ message: err.message || "An unknown error occurred." });
 });
 
-const port = process.env.PORT || 5001;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
