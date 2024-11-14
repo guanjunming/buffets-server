cbd2192a8b036b56e5c12a134550446b9036bb5f
Guan Junming <guan.junming85@gmail.com>
2024-11-14 14:50:09 +0800

    add auth input validation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,26 @@ const express = require("express");
 const authController = require("../controllers/authController");
 const catchAsync = require("../utils/catchAsync");
 const checkAuth = require("../middleware/checkAuth");
+const {
+  validateSignupData,
+  validateLoginData,
+} = require("../validators/userValidators");
+const checkErrors = require("../validators/checkErrors");
 
 const router = express.Router();
 
-router.post("/signup", catchAsync(authController.signup));
-router.post("/login", catchAsync(authController.login));
+router.post(
+  "/signup",
+  validateSignupData,
+  checkErrors,
+  catchAsync(authController.signup)
+);
+router.post(
+  "/login",
+  validateLoginData,
+  checkErrors,
+  catchAsync(authController.login)
+);
 router.post("/refresh", authController.refresh);
 
 module.exports = router;
