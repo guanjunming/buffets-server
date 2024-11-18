3eca7737d10cfe6ca5723b4a0eb99efc22c4f700
eeexuanlyx <eeexuanlyx@gmail.com>
2024-11-18 19:20:06 +0800

    add remove fav function

--- controllers/favouriteController.js
+++ controllers/favouriteController.js
@@ -1,61 +1,73 @@
 const Restaurant = require("../models/Restaurant");
 const User = require("../models/User");
+const CustomError = require("../utils/customError");
 
 const addFavourites = async (req, res) => {
   const userId = req.user.id;
   const { id: restaurantId } = req.params;
 
-  console.log("userId:", userId);
-  console.log(req.user);
-  console.log("restaurantId:", restaurantId);
-
   try {
     const restaurant = await Restaurant.findById(restaurantId);
     if (!restaurant) {
-      console.log("Restaurant not found:", restaurantId);
-      return res.status(404).json({ message: "Restaurant not found" });
+      return next(new CustomError("No restaurant found with provided id", 404));
     }
 
     const user = await User.findById(userId);
     if (!user.favourites.includes(restaurantId)) {
       user.favourites.push(restaurantId);
       await user.save();
-      console.log("Restaurant added to favorites:", restaurantId);
+
       return res.status(200).json({ message: "Restaurant added to favorites" });
     } else {
-      console.log("Restaurant already in favorites:", restaurantId);
-      return res
-        .status(400)
-        .json({ message: "Restaurant already in favorites" });
+      return next(new CustomError("Restaurant already in favourites", 400));
     }
   } catch (error) {
-    console.error("Error in addFavourites:", error);
-    return res
-      .status(500)
-      .json({ message: "Server error", error: error.message || error });
+    return next(new CustomError("Failed to add favourites", 500));
   }
 };
 
 const getAllFavourites = async (req, res) => {
   try {
     const userId = req.user.id;
-    console.log("userId:", userId);
-    console.log(req.user);
 
     const user = await User.findById(userId).populate("favourites");
 
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return next(new CustomError("User not found", 404));
     }
 
     return res.status(200).json(user.favourites);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "Server error" });
+    return next(new CustomError("Failed to get favourites", 500));
+  }
+};
+
+const removeFavourite = async (req, res) => {
+  const userId = req.user.id;
+  const { id: restaurantId } = req.params;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return next(new CustomError("User not found", 404));
+    }
+
+    user.favourites = user.favourites.filter(
+      (fav) => fav.toString() !== restaurantId
+    );
+
+    await user.save();
+
+    res.status(200).json({ message: "Restaurant removed from favourites" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
   }
 };
 
 module.exports = {
   addFavourites,
   getAllFavourites,
+  removeFavourite,
 };
--- routes/favouriteRoutes.js
+++ routes/favouriteRoutes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   addFavourites,
   getAllFavourites,
+  removeFavourite,
 } = require("../controllers/favouriteController");
 const checkAuth = require("../middleware/checkAuth");
 const router = express.Router();
 
 router.post("/:id", checkAuth, addFavourites);
 router.get("/", checkAuth, getAllFavourites);
+router.delete("/:id", checkAuth, removeFavourite);
 
 module.exports = router;
