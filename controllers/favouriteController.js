7f228c2a9d535a69b7abd44f95b7ad129e04ae6c
Guan Junming <guan.junming85@gmail.com>
2024-11-19 11:07:24 +0800

    update favorites controller, populate reviews for get favorites

--- controllers/favouriteController.js
+++ controllers/favouriteController.js
@@ -2,55 +2,59 @@ const Restaurant = require("../models/Restaurant");
 const User = require("../models/User");
 const CustomError = require("../utils/customError");
 
-const addFavourites = async (req, res) => {
-  const userId = req.user.id;
-  const { id: restaurantId } = req.params;
-
+const getAllFavourites = async (req, res, next) => {
   try {
+    const user = await User.findById(req.user.id).populate({
+      path: "favourites",
+      populate: { path: "reviews", select: "rating" },
+    });
+
+    if (!user) {
+      return next(new CustomError("No user found with provided id.", 404));
+    }
+
+    return res.json(user.favourites);
+  } catch (error) {
+    return next(new CustomError("Failed to get favourites.", 500));
+  }
+};
+
+const addFavourites = async (req, res, next) => {
+  try {
+    const { id: restaurantId } = req.params;
+
     const restaurant = await Restaurant.findById(restaurantId);
     if (!restaurant) {
-      return next(new CustomError("No restaurant found with provided id", 404));
+      return next(
+        new CustomError("No restaurant found with provided id.", 404)
+      );
     }
 
-    const user = await User.findById(userId);
-    if (!user.favourites.includes(restaurantId)) {
-      user.favourites.push(restaurantId);
-      await user.save();
-
-      return res.status(200).json({ message: "Restaurant added to favorites" });
-    } else {
-      return next(new CustomError("Restaurant already in favourites", 400));
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return next(new CustomError("No user found with provided id.", 404));
     }
+
+    if (user.favourites.includes(restaurantId)) {
+      return next(new CustomError("Restaurant already in favourites.", 400));
+    }
+
+    user.favourites.push(restaurantId);
+    await user.save();
+
+    return res.json({ message: "Restaurant added to favorites." });
   } catch (error) {
-    return next(new CustomError("Failed to add favourites", 500));
+    return next(new CustomError("Failed to add favourites.", 500));
   }
 };
 
-const getAllFavourites = async (req, res) => {
+const removeFavourite = async (req, res, next) => {
   try {
-    const userId = req.user.id;
+    const { id: restaurantId } = req.params;
 
-    const user = await User.findById(userId).populate("favourites");
-
+    const user = await User.findById(req.user.id);
     if (!user) {
-      return next(new CustomError("User not found", 404));
-    }
-
-    return res.status(200).json(user.favourites);
-  } catch (error) {
-    console.error(error);
-    return next(new CustomError("Failed to get favourites", 500));
-  }
-};
-
-const removeFavourite = async (req, res) => {
-  const userId = req.user.id;
-  const { id: restaurantId } = req.params;
-
-  try {
-    const user = await User.findById(userId);
-    if (!user) {
-      return next(new CustomError("User not found", 404));
+      return next(new CustomError("No user found with provided id.", 404));
     }
 
     user.favourites = user.favourites.filter(
@@ -59,10 +63,9 @@ const removeFavourite = async (req, res) => {
 
     await user.save();
 
-    res.status(200).json({ message: "Restaurant removed from favourites" });
+    res.json({ message: "Restaurant removed from favourites." });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
+    return next(new CustomError("Failed to remove from favourites.", 500));
   }
 };
 
